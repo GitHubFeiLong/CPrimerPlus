97c240f259f9e3f249cfe8cab637d113e1f9f192
陈飞龙 <1696741038@qq.com>
2023-11-26 20:40:22 +0800

    第九章复习题

---
+++ review_9_9.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void showmenu(void);
+int getchoice(int min, int max);
+
+int main(void)
+{
+	int min, max;
+	int r;
+
+	showmenu();
+	while ((r = getchoice(1, 4)) != 4)
+	{
+		printf("ÏÔÊ¾²Ëµ¥£º\n");
+		showmenu();
+	}
+	printf("Bye!\n");
+
+	return 0;
+}
+
+void showmenu(void)
+{
+	int choice;
+	int status;
+
+	printf("Please choose one of the following:\n");
+	printf("1) copy files		2)move files\n");
+	printf("3) remove files		4)quit\n");
+	printf("Enter the number of your choice:\n");
+}
+
+int getchoice(int min, int max)
+{
+	int i;
+	int flag;
+
+	printf("Enter the number:\n");
+	flag = scanf("%d", &i);
+	while (flag && !(i >= min || i <= max))
+	{
+		showmenu();
+		flag = scanf("%d", &i);
+	}
+	// ¶ÁÈ¡Ê£ÓàµÄ×Ö·û
+	if (flag != 1)
+		return 4;
+
+	return i;
+}
